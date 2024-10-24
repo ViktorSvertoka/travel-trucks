d0e12afd67b44b92d6671ad918591c86f0513056
Viktor Svertoka <victor.svertoka@gmail.com>
2024-10-24 21:53:16 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        heroDesktop: `url(./src/images/hero.png)`,
+      },
+      screens: {
+        xs: "375px",
+        sm: "834px",
+        lg: "1440px",
+      },
+      colors: {
+        main: "#101828",
+        text: "#475467",
+        white: "#ffffff",
+        inputs: "#f7f7f7",
+        badges: "#f2f4f7",
+        gray: "#6c717b",
+        grayLight: "#dadde1",
+        button: "#e44848",
+        buttonHover: "#d84343",
+        rating: "#ffc531",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
