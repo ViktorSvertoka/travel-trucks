71aa6e376d8cb2f8b909421230e8fd428dd64c4c
Viktor Svertoka <victor.svertoka@gmail.com>
2024-10-24 23:01:02 +0300

    Update NotFound.jsx

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,17 +1,23 @@
+import { Helmet } from "react-helmet-async";
 import notFoundImage from "../../images/404.png";
 
 const NotFound = () => {
   return (
-    <div className="flex flex-col items-center justify-center text-4xl font-semibold mt-[100px]">
-      <p>Oops! Page not found...</p>
-      <img
-        className="mt-[50px]"
-        src={notFoundImage}
-        alt="Page 404"
-        width={400}
-        height={400}
-      />
-    </div>
+    <>
+      <Helmet>
+        <title>Page 404</title>
+      </Helmet>
+      <div className="flex flex-col items-center justify-center text-4xl font-semibold mt-[100px]">
+        <p>Oops! Page not found...</p>
+        <img
+          className="mt-[50px]"
+          src={notFoundImage}
+          alt="Page 404"
+          width={400}
+          height={400}
+        />
+      </div>
+    </>
   );
 };
 
