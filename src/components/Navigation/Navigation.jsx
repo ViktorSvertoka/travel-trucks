d604ebdcd01340ea3dfa554058e69fe3433a3f0b
Viktor Svertoka <victor.svertoka@gmail.com>
2024-10-26 16:27:00 +0300

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,14 +3,13 @@ import clsx from "clsx";
 
 const activeLink = ({ isActive }) => {
   return clsx(
-    "text-main transition-transform duration-250 ease-in-out hover:scale-105",
     isActive ? "text-buttonHover !text-buttonHover" : "hover:text-buttonHover"
   );
 };
 
 const Navigation = () => {
   return (
-    <nav className="flex gap-8 justify-start font-medium text-lg absolute left-1/2 transform -translate-x-1/2">
+    <nav className="flex gap-8 justify-start font-medium text-main text-lg absolute left-1/2 transform -translate-x-1/2">
       <NavLink to="/" className={activeLink}>
         Home
       </NavLink>
