b959db4fd14180128d00d16222b83507994c6881
Viktor Svertoka <victor.svertoka@gmail.com>
2024-11-03 10:07:20 +0200

    Feat(SVG) Added image to page 404

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,5 @@
 import { Helmet } from "react-helmet-async";
-import notFoundImage from "../../images/404.png";
+import NotFoundImage from "../../images/404.svg?react";
 
 const NotFound = () => {
   return (
@@ -9,12 +9,11 @@ const NotFound = () => {
       </Helmet>
       <div className="flex flex-col items-center justify-center text-4xl font-semibold mt-[100px]">
         <p>Oops! Page not found...</p>
-        <img
+        <NotFoundImage
           className="mt-[50px]"
-          src={notFoundImage}
-          alt="Page 404"
           width={400}
           height={400}
+          aria-label="Page not found 404"
         />
       </div>
     </>
